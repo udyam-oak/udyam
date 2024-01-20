990e0b6c6c792c6c7441fb0d4fe225e81deeceb9
KBrijeshReddy <brijeshreddy12@gmail.com>
2024-01-20 21:54:27 +0530

    MaketplaceNew
    -brijesh

--- frontend/src/components/MarketplaceCard.tsx
+++ frontend/src/components/MarketplaceCard.tsx
@@ -6,7 +6,7 @@ const MarketplaceCard = ({image, cardname, price}) => {
   return (
     <>
     <div className='border border-[#263238] w-40 h-56 rounded-xl m-16 text-center'>
-        <img src={image} alt="" className='rounded-t-xl h-32 bg-slate-100'/>
+        <img src={image} alt="" className='rounded-t-xl h-32 bg-slate-200'/>
         <span className=''>{cardname}</span><br />
         <span className=' font-semibold'>{price}</span><br />
         <button className='mt-2 rounded-md hover:bg-[#BA68C8] bg-[#7D538D] text-white p-1 duration-300'>&nbsp;BUY&nbsp;</button>
