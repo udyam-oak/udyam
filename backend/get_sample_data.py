65afd1affceefe938aa17bd4f22916bb59fe38f7
Absolute Reality <89690272+AbsoluteReality@users.noreply.github.com>
2024-01-20 17:16:45 +0530

    Added challenges backend (#13)
    * fixed issues in auth, removed connection with atlas and created get_sample_data.py
    
    * created backend for challenges
    
    except for leaderboard
    
    * added gitignore for pycache

--- backend/get_sample_data.py
+++ backend/get_sample_data.py
@@ -6,10 +6,90 @@ user_credentials = {
     "Joe": "BackwaterBliss42"
 }
 
+challenges = [
+  {
+  "challenge_id": 1,
+  "title": "Maths",
+  "easy": {
+    "If 1 is a root of the quadratic equation 3x2 + ax - 2 = 0 and the quadratic equation a (x2 + 6x) - b = 0 has equal roots, find the value of b." : 9,
+    "A motor boat whose speed is 18 km/hr in still water takes 1 hr more to go 24 km upstream than to return downstream to the same spot. Find the speed of the stream in" : 6,
+    "Find the common difference of an AP whose first term is 4, the last term is 49 and the sum of all its terms is 265." : 5,
+    "A ladder, leaning against a wall, makes an angle of 600 with the horizontal. If the foot of the ladder is 2.5 m away from the wall, find the length of the ladder." : 5
+    },
+
+  "medium": {
+    "How many terms of an AP 9, 17, 25, … must be taken to give a sum of 636?" : 12,
+    "A cone of height 20 cm and radius of base 5 cm is made up of modeling clay. A child reshapes it in the form of a sphere. Find the diameter of the sphere" : 10,
+    "A solid sphere of diameter 6 cm is dropped in a right circular cylindrical vessel partly filled with water. The diameter of the cylindrical vessel is 12 cm. If the sphere is completely submerged in water, by how much will the level of water rise in the cylindrical vessel? " : 1,
+    "A conical vessel, with base radius 5 cm and height 24 cm, is full of water. This water is emptied into cylindrical vessel of base radius 10 cm. Find the height to which the water will rise in the cylindrical vessel" : 2
+    },
+
+  "hard": {
+    "Water is flowing at the rate of 2.52 kh/h through a cylindrical pipe into a cylindrical tank, the radius of whose base is 40 cm, if the increase in the level of water in the tank in half an hour is 3.15 m, find the internal diameter of the pipe." : 4,
+    "Water running in a cylindrical pipe of inner diameter 7 cm, is collected in a container at the rate of 182.5 litres per minute. Find the rate of flow of water in the pipe in km/h" : 3,
+    "Water is flowing at the rate of 15 km/h through a pipe of diameter 14 cm into a cuboidal pond which is 50m long and 44 m wide. In what time will the level of water in the pond rise by 21 cm?" : 2,
+    "Volume and surface area of a solid hemisphere are numerically equal. What is the diameter of hemisphere?" : 9
+    }
+  },
+  {
+  "challenge_id": 2,
+  "title": "Maths",
+  "easy": {
+    "If 1 is a root of the quadratic equation 3x2 + ax - 2 = 0 and the quadratic equation a (x2 + 6x) - b = 0 has equal roots, find the value of b." : 9,
+    "A motor boat whose speed is 18 km/hr in still water takes 1 hr more to go 24 km upstream than to return downstream to the same spot. Find the speed of the stream in" : 6,
+    "Find the common difference of an AP whose first term is 4, the last term is 49 and the sum of all its terms is 265." : 5,
+    "A ladder, leaning against a wall, makes an angle of 600 with the horizontal. If the foot of the ladder is 2.5 m away from the wall, find the length of the ladder." : 5
+    },
+
+  "medium": {
+    "How many terms of an AP 9, 17, 25, … must be taken to give a sum of 636?" : 12,
+    "A cone of height 20 cm and radius of base 5 cm is made up of modeling clay. A child reshapes it in the form of a sphere. Find the diameter of the sphere" : 10,
+    "A solid sphere of diameter 6 cm is dropped in a right circular cylindrical vessel partly filled with water. The diameter of the cylindrical vessel is 12 cm. If the sphere is completely submerged in water, by how much will the level of water rise in the cylindrical vessel? " : 1,
+    "A conical vessel, with base radius 5 cm and height 24 cm, is full of water. This water is emptied into cylindrical vessel of base radius 10 cm. Find the height to which the water will rise in the cylindrical vessel" : 2
+    },
+
+  "hard": {
+    "Water is flowing at the rate of 2.52 kh/h through a cylindrical pipe into a cylindrical tank, the radius of whose base is 40 cm, if the increase in the level of water in the tank in half an hour is 3.15 m, find the internal diameter of the pipe." : 4,
+    "Water running in a cylindrical pipe of inner diameter 7 cm, is collected in a container at the rate of 182.5 litres per minute. Find the rate of flow of water in the pipe in km/h" : 3,
+    "Water is flowing at the rate of 15 km/h through a pipe of diameter 14 cm into a cuboidal pond which is 50m long and 44 m wide. In what time will the level of water in the pond rise by 21 cm?" : 2,
+    "Volume and surface area of a solid hemisphere are numerically equal. What is the diameter of hemisphere?" : 9
+    }
+  },
+  {
+  "challenge_id": 3,
+  "title": "Maths",
+  "easy": {
+    "If 1 is a root of the quadratic equation 3x2 + ax - 2 = 0 and the quadratic equation a (x2 + 6x) - b = 0 has equal roots, find the value of b." : 9,
+    "A motor boat whose speed is 18 km/hr in still water takes 1 hr more to go 24 km upstream than to return downstream to the same spot. Find the speed of the stream in" : 6,
+    "Find the common difference of an AP whose first term is 4, the last term is 49 and the sum of all its terms is 265." : 5,
+    "A ladder, leaning against a wall, makes an angle of 600 with the horizontal. If the foot of the ladder is 2.5 m away from the wall, find the length of the ladder." : 5
+    },
+
+  "medium": {
+    "How many terms of an AP 9, 17, 25, … must be taken to give a sum of 636?" : 12,
+    "A cone of height 20 cm and radius of base 5 cm is made up of modeling clay. A child reshapes it in the form of a sphere. Find the diameter of the sphere" : 10,
+    "A solid sphere of diameter 6 cm is dropped in a right circular cylindrical vessel partly filled with water. The diameter of the cylindrical vessel is 12 cm. If the sphere is completely submerged in water, by how much will the level of water rise in the cylindrical vessel? " : 1,
+    "A conical vessel, with base radius 5 cm and height 24 cm, is full of water. This water is emptied into cylindrical vessel of base radius 10 cm. Find the height to which the water will rise in the cylindrical vessel" : 2
+    },
+
+  "hard": {
+    "Water is flowing at the rate of 2.52 kh/h through a cylindrical pipe into a cylindrical tank, the radius of whose base is 40 cm, if the increase in the level of water in the tank in half an hour is 3.15 m, find the internal diameter of the pipe." : 4,
+    "Water running in a cylindrical pipe of inner diameter 7 cm, is collected in a container at the rate of 182.5 litres per minute. Find the rate of flow of water in the pipe in km/h" : 3,
+    "Water is flowing at the rate of 15 km/h through a pipe of diameter 14 cm into a cuboidal pond which is 50m long and 44 m wide. In what time will the level of water in the pond rise by 21 cm?" : 2,
+    "Volume and surface area of a solid hemisphere are numerically equal. What is the diameter of hemisphere?" : 9
+    }
+  }
+]
+
 from pymongo import MongoClient
 from bcrypt import hashpw, gensalt
 
 db = MongoClient().udyam
 
 for name, password in user_credentials.items():
-  db.users.insert_one({"name": name, "password": hashpw(password.encode("utf-8"), gensalt())})
+  db.users.insert_one({"name": name,
+                       "password": hashpw(password.encode("utf-8"), gensalt()),
+                       "avatar_url": "",
+                       "challenges_attempted": []})
+
+db.challenges.insert_many(challenges)
