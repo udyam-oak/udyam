d81ff523b3373332fc10a37c248d7d1807941c0d
KBrijeshReddy <brijeshreddy12@gmail.com>
2024-01-20 18:17:12 +0530

    Homepage Done
    -brijesh

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Login = () => {
   return (
     <>
-    <div className="flex justify-between bg-[#E0BBE6] font-poppins">
+    <div className="flex justify-between font-poppins bg-slate-100 text-[#263238]">
         <div className='bg-[#BA68C8] h-screen w-1/3'>
             <img src="src\assets\Image--.png" alt="" className='translate-x-1/2 translate-y-32' id='login-page-image' />
         </div>
@@ -11,10 +11,10 @@ const Login = () => {
             <h1 className="text-6xl m-4">LOGIN</h1><br />
             <form action="">
                 <label htmlFor="" className='text-xl my-2 ml-2'>Username</label><br />
-                <input type="text" name="name" className='border-2 border-[#7D538D] rounded text-3xl mt-2 h-11' /><br /><br/><br/>
-                <label htmlFor="" className='text-xl ml-2 mt-40'>Password</label><br />
-                <input type="password" name="password" className='border-2 border-[#7D538D] rounded text-3xl mt-2 h-11' /><br />
-                <input type="submit" className='cursor-pointer hover:bg-[#BA68C8] duration-500 text-xl mt-9 ml-28 bg-[#7D538D] px-10 py-2 rounded-xl text-white' value={"LOGIN"}/><br/>
+                <input type="text" name="name" className='border-2 border-[#7D538D] rounded text-3xl mt-2 h-11 w-96 ' /><br /><br/><br/>
+                <label htmlFor="" className='text-xl ml-2 mt-36'>Password</label><br />
+                <input type="password" name="password" className='border-2 border-[#7D538D] rounded text-3xl mt-2 h-11 w-96 ' /><br />
+                <input type="submit" className='cursor-pointer hover:bg-[#BA68C8] duration-500 text-xl mt-12 bg-[#7D538D] px-10 py-2 rounded text-white w-96' value={"LOGIN"}/><br/>
             </form>
         </div>
     </div>
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,14 +6,14 @@ const Navbar = () => {
   <>
   <nav className="flex items-center justify-between text-black h-14 font-poppins" >
     <div className="flex items-center">
-        <img className="mx-3 w-10" src='src\assets\insight.png'/>
+        <img className="mx-3 w-10" src='src\assets\analyzer.png'/>
         <span className='font-bold text-xl text-[#263238]'>UDYAM</span>
         </div>
         <ul className='flex'>
-            <li className=" text-[#263238] mx-3 p-2 duration-300 hover:text-[#263238] hover:bg-[#E0BBE6] cursor-pointer rounded-lg"><Link className='hover:text-[#263238] duration-300 hover:bg-[#E0BBE6] rounded-lg' to="/">Home</Link></li>
-            <li className=" text-[#263238] mx-3 p-2 duration-300 hover:text-[#263238] hover:bg-[#E0BBE6] cursor-pointer rounded-lg"><Link className='hover:text-[#263238] duration-300 hover:bg-[#E0BBE6] rounded-lg' to="/challenge">Challenges</Link></li>
-            <li className=" text-[#263238] mx-3 p-2 duration-300 hover:text-[#263238] hover:bg-[#E0BBE6] cursor-pointer rounded-lg"><Link className='hover:text-[#263238] duration-300 hover:bg-[#E0BBE6] rounded-lg' to="/marketplace">Marketplace</Link></li>
-            <li className=" text-[#263238] mx-3 p-2 duration-300 hover:text-[#263238] hover:bg-[#E0BBE6] cursor-pointer rounded-lg"><Link className='hover:text-[#263238] duration-300 hover:bg-[#E0BBE6] rounded-lg' to="/login">Login</Link></li>
+            <li className=" text-[#263238] mx-3 p-2 duration-300 hover:text-[#263238] hover:bg-[#E0BBE6] cursor-pointer rounded-sm"><Link className='hover:text-[#263238] duration-300 hover:bg-[#E0BBE6] rounded-sm' to="/">Home</Link></li>
+            <li className=" text-[#263238] mx-3 p-2 duration-300 hover:text-[#263238] hover:bg-[#E0BBE6] cursor-pointer rounded-sm"><Link className='hover:text-[#263238] duration-300 hover:bg-[#E0BBE6] rounded-sm' to="/challenge">Challenges</Link></li>
+            <li className=" text-[#263238] mx-3 p-2 duration-300 hover:text-[#263238] hover:bg-[#E0BBE6] cursor-pointer rounded-sm"><Link className='hover:text-[#263238] duration-300 hover:bg-[#E0BBE6] rounded-sm' to="/marketplace">Marketplace</Link></li>
+            <li className=" text-[#263238] mx-3 p-2 duration-300 hover:text-[#263238] hover:bg-[#E0BBE6] cursor-pointer rounded-sm"><Link className='hover:text-[#263238] duration-300 hover:bg-[#E0BBE6] rounded-sm' to="/login">Login</Link></li>
         </ul>
     </nav>
   </>
