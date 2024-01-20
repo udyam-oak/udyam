67c47ee520883e1e7cbaa4922c62997523e2f2fb
MrBlackBed05 <mrblackbed@gmail.com>
2024-01-20 13:53:36 +0530

    Redoing because an error

--- backend/application/marketplace.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, session, redirect, url_for, request, jsonify
-from flask_cors import cross_origin
-from auth import db
-from .database import mongo
-
-marketplace = Blueprint("marketplace", __name__)
-
