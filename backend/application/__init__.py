dd17f109666825482b79e3d1f306b1554a3dbc43
MrBlackBed05 <mrblackbed@gmail.com>
2024-01-20 13:42:25 +0530

    Added marketplace python file for the marketplace

--- backend/application/__init__.py
+++ backend/application/__init__.py
@@ -5,13 +5,16 @@ from .database import mongo
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "sampleSecretKey"
-    app.config["MONGO_URI"] =  "mongodb+srv://admin:admin@cluster0.7jz4qws.mongodb.net/" # "mongodb://localhost:27017/udyam"
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/udyam" #"mongodb+srv://admin:admin@cluster0.7jz4qws.mongodb.net/" 
     mongo.init_app(app)
     CORS(app)
 
     from .views import views
     from .auth import auth
+    from .marketplace import marketplace
 
+
+    app.register_blueprint(marketplace, url_prefix='/')
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
