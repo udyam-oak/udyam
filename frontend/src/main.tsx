4f0f4b13be5130a7a5f6e84c7681d612e24e0be4
KBrijeshReddy <125353127+KBrijeshReddy@users.noreply.github.com>
2024-01-20 11:50:04 +0530

    feat: NavBar and Login Page made (#1)
    feat

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,16 +6,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Challenge from "./components/Challenge.tsx";
 import Marketplace from "./components/Marketplace.tsx";
 import Navbar from "./components/Navbar.tsx";
+import Login from "./components/Login.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+    <Navbar />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/challenge" element={<Challenge />} />
         <Route path="/marketplace" element={<Marketplace />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
-      <Navbar />
     </BrowserRouter>
   </React.StrictMode>
 );
