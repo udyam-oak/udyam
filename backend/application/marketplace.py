39195ffde9f72cddcf0629b143a04f8d3be6a98d
Prashant <67903361+prsh11@users.noreply.github.com>
2024-01-21 05:16:49 +0530

    feat: powerup integration (#26)
    * add connection
    
    * complete powerups

--- backend/application/marketplace.py
+++ backend/application/marketplace.py
@@ -62,19 +62,43 @@ def buy():
     else:
         return jsonify({"message": "Insufficient points or user not found."})
     
+<<<<<<< HEAD
+
+@marketplace.route("/getUserItems", methods=['GET'])
+@cross_origin()
+def get_user_item_count():
+    user_name = request.args.get('name')  # Adjust the parameter based on your API design
+    item_name = request.args.get('item')  # Get the name of the item you're interested in
+=======
 @marketplace.route("/get_user_items", methods=['GET'])
 @cross_origin()
 def get_user_items():
     user_name = request.args.get('name')  # Adjust the parameter based on your API design
+>>>>>>> master
 
     # Fetch the document for the user based on the provided name
     user_document = db.users.find_one({"name": user_name})
 
     # Check if the user document exists
     if user_document:
+<<<<<<< HEAD
+        # Get the user's items list
+        user_items = user_document.get("items", [])
+
+        # Initialize item count to 0
+        item_count = 0
+
+        # Iterate through the user's items and count occurrences of the specified item
+        for item in user_items:
+            if item.get("item") == item_name:
+                item_count += 1
+
+        return jsonify({"item_count": item_count})
+=======
         # Get the user's items dictionary
         user_items = user_document.get("items", {})
 
         return jsonify({"user_items": user_items})
+>>>>>>> master
     else:
         return jsonify({"message": "User not found."})
--- frontend/src/components/Question.tsx
+++ frontend/src/components/Question.tsx
@@ -4,6 +4,8 @@ import { useParams } from "react-router-dom";
 import { toast } from "sonner";
 import { LuTarget } from "react-icons/lu";
 import { useLocalStorage } from "@uidotdev/usehooks";
+import coinBooster from "../assets/coin_booster.png";
+import insurance from "../assets/insurance.png";
 
 const Question = () => {
   const { id } = useParams();
@@ -11,12 +13,55 @@ const Question = () => {
   const [input, setInput] = useState("");
   const [score, setScore] = useState(0);
   const [q, setQ] = useState(0);
+  const [multiplier, setMulti] = useState(1);
+  const [coinB, setCoinBoost] = useState(0);
   const [points, setPoints] = useState(0);
 
   const [time, setTime] = useState(0);
   const [isRunning, setRuning] = useState(false);
   const [user, setUser] = useLocalStorage("username", "");
+  const [insur, setInsur] = useState(false);
+  const [insuC, setInsuC] = useState(0);
 
+  const insuranceF = () => {
+    axios
+      .get("http://127.0.0.1:5000/getUserItems", {
+        params: {
+          name: user,
+          item: "insurance",
+        },
+      })
+      .then((res) => {
+        setInsuC(res.data.item_count);
+      });
+  };
+  const coinBoost = () => {
+    axios
+      .get("http://127.0.0.1:5000/getUserItems", {
+        params: {
+          name: user,
+          item: "coin_booster",
+        },
+      })
+      .then((res) => {
+        setCoinBoost(res.data.item_count);
+      });
+    toast.success("Activated 2x coin mode");
+  };
+  useEffect(() => {
+    if (coinB > 0) {
+      setMulti(2);
+    } else {
+      toast.error("You don't have any");
+    }
+  }, [coinB]);
+  useEffect(() => {
+    if (insuC > 0) {
+      setInsuC(true);
+    } else {
+      toast.error("You don't have any");
+    }
+  }, [insuC]);
   const getData = () => {
     axios
       .get("http://127.0.0.1:5000/getQuestions", {
@@ -41,6 +86,11 @@ const Question = () => {
       toast.success(`You scored ${score}/${Object.keys(question).length}`);
       setRuning(false);
       sendData();
+    }
+    if (insur) {
+      toast.success("You have gotten a second try");
+      setInsur(false);
+      return;
     }
     setQ(q + 1);
     setInput("");
@@ -76,7 +126,7 @@ const Question = () => {
       params: {
         challenge_id: id,
         name: user,
-        points: 5 * score,
+        points: 5 * score * multiplier,
         time_taken: time,
         date_attempted: getFormattedDate(),
       },
@@ -128,10 +178,20 @@ const Question = () => {
                 <LuTarget size="300px" />
                 <div className="text-2xl">
                   You Completed it {score}/{Object.keys(question).length}
+                  <br />
+                  {5 * score * multiplier}
                 </div>
                 <div>Time taken {time}</div>
               </div>
             )}
+        </div>
+      </div>
+      <div className="flex gap-4 mt-10">
+        <div className="w-[50px] h-[50px] cursor-pointer" onClick={coinBoost}>
+          <img src={coinBooster} alt="coin booster" />
+        </div>
+        <div className="w-[50px] h-[50px] cursor-pointer" onClick={insuranceF}>
+          <img src={insurance} alt="insurace" />
         </div>
       </div>
     </div>
