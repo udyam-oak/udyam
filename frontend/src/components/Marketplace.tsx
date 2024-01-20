3ee948f18bd417d88808f81a959e29525a610346
KBrijeshReddy <brijeshreddy12@gmail.com>
2024-01-20 13:49:36 +0530

    MarketPlace Added
    -brijesh

--- frontend/src/components/Marketplace.tsx
+++ frontend/src/components/Marketplace.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import MarketplaceCard from "./MarketplaceCard";
 
 const Marketplace = () => {
-  return <div>Marketplace</div>;
+  return(
+    <>
+    <div className="flex flex-wrap items-center justify-center">
+    <MarketplaceCard price="$ 250 pts" image="$nbsp;" cardname="Coin UnBoost"/>
+    <MarketplaceCard price="$ 250 pts" image="$nbsp;" cardname="Coin UnBoost"/>
+    <MarketplaceCard price="$ 250 pts" image="$nbsp;" cardname="Coin UnBoost"/> 
+    <MarketplaceCard price="$ 250 pts" image="$nbsp;" cardname="Coin UnBoost"/>
+    <MarketplaceCard price="$ 250 pts" image="$nbsp;" cardname="Coin UnBoost"/>
+    <MarketplaceCard price="$ 250 pts" image="$nbsp;" cardname="Coin UnBoost"/>
+    <MarketplaceCard price="$ 250 pts" image="$nbsp;" cardname="Coin UnBoost"/>
+    <MarketplaceCard price="$ 250 pts" image="$nbsp;" cardname="Coin UnBoost"/>
+    <MarketplaceCard price="$ 250 pts" image="$nbsp;" cardname="Coin UnBoost"/>
+    <MarketplaceCard price="$ 250 pts" image="$nbsp;" cardname="Coin UnBoost"/>
+
+    </div>
+    </>
+  );
 };
 
 export default Marketplace;
+
+// grid grid-cols-4 place-items-center gap-10 place-content-evenly
