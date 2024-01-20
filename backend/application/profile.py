6d21346ebc31ad4dad44eced4bd0409ea7f1e467
MrBlackBed05 <mrblackbed@gmail.com>
2024-01-20 16:34:39 +0530

    changed cuz missed some bracks0

--- backend/application/profile.py
+++ backend/application/profile.py
@@ -7,8 +7,8 @@ profile = Blueprint("profile", __name__)
 
 db = mongo
 
-@profile.route('setAvatar', methods=['GET'])
-@cross_origin
+@profile.route('/setAvatar', methods=['GET'])
+@cross_origin()
 def setAvatar():
     url = request.args.get('avatar_url')
     uname = request.args.get('username')
