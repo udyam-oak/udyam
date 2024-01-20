03824ca0c13d1ee76d8d81eb313e4a1861188c6f
KBrijeshReddy <125353127+KBrijeshReddy@users.noreply.github.com>
2024-01-20 13:50:40 +0530

    feat: MarketPlace Added (#3)
    -brijesh

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,8 +5,10 @@ const Navbar = () => {
   return (
   <>
   <nav className="flex items-center justify-between text-black h-14 font-poppins" >
-        {/* <img className="mx-3 font-bold text-xl w-20" src='src\assets\LOGO.png'/> */}
-        <span className='mx-3 font-bold text-xl text-[#263238]'>UDYAM</span>
+    <div className="flex items-center">
+        <img className="mx-3 w-10" src='src\assets\insight.png'/>
+        <span className='font-bold text-xl text-[#263238]'>UDYAM</span>
+        </div>
         <ul className='flex'>
             <li className=" text-[#263238] mx-3 p-2 duration-300 hover:text-[#263238] hover:bg-[#E0BBE6] cursor-pointer rounded-lg"><Link className='hover:text-[#263238] duration-300 hover:bg-[#E0BBE6] rounded-lg' to="/">Home</Link></li>
             <li className=" text-[#263238] mx-3 p-2 duration-300 hover:text-[#263238] hover:bg-[#E0BBE6] cursor-pointer rounded-lg"><Link className='hover:text-[#263238] duration-300 hover:bg-[#E0BBE6] rounded-lg' to="/challenge">Challanges</Link></li>
