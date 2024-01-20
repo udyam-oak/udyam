74cb92ab5b13e25b6710c50d3a0aaf5f31f7af26
Prashant Baid <prashantbaid80@gmail.com>
2024-01-20 12:36:38 +0530

    add basic avatarw

---
+++ frontend/src/components/Profile.tsx
@@ -0,0 +1,175 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import HeatMap from "@uiw/react-heat-map";
+import { useState } from "react";
+import { Avatar, FloorReflection } from "@readyplayerme/visage";
+import { AvatarCreatorConfig } from "@readyplayerme/react-avatar-creator";
+
+const value = [
+  { date: "2023/01/11", count: 2 },
+  { date: "2023/01/12", count: 4 },
+  { date: "2023/01/13", count: 10 },
+  ...[...Array(17)].map((_, idx) => ({
+    date: `2023/02/${idx + 10}`,
+    count: idx,
+    content: "",
+  })),
+  { date: "2023/04/11", count: 2 },
+  { date: "2023/05/01", count: 5 },
+  { date: "2023/05/02", count: 5 },
+  { date: "2023/05/04", count: 11 },
+];
+
+const config: AvatarCreatorConfig = {
+  clearCache: true,
+  bodyType: "fullbody",
+  quickStart: true,
+  language: "en",
+};
+const Profile = () => {
+  const { id } = useParams();
+  const [avatar, setAvatar] = useState(
+    "https://models.readyplayer.me/65a7d3b2c2a12602176abe16.glb"
+  );
+  const [avatarCreator, setAvatarCreator] = useState(false);
+  return (
+    <>
+      <div className="flex gap-10 w-screen h-screen justify-center mt-10">
+        <div>
+          <div className="w-[500px] h-[500px] bg-[#EBEBEB]">
+            <Avatar
+              animationSrc="https://readyplayerme.github.io/visage/male-idle.glb"
+              backLightColor="#FFB878"
+              backLightIntensity={2.2}
+              background={{
+                color: "rgb(9,20,26)",
+              }}
+              bloom={{
+                intensity: 0.1,
+                kernelSize: 1,
+                luminanceSmoothing: 1,
+                luminanceThreshold: 1,
+                materialIntensity: 3.3,
+                mipmapBlur: true,
+              }}
+              blur={[300, 200]}
+              cameraInitialDistance={3.2}
+              cameraTarget={1.55}
+              color="rgb(9,20,26)"
+              depthScale={1.2}
+              depthToBlurRatioBias={1}
+              distortion={0}
+              effects={{
+                ambientOcclusion: true,
+              }}
+              environment="warehouse"
+              fillLightColor="#6794FF"
+              fillLightIntensity={0.8}
+              fov={50}
+              keyLightColor="#FFFFFF"
+              keyLightIntensity={1.2}
+              maxDepthThreshold={1.4}
+              metalness={0.5}
+              minDepthThreshold={0.4}
+              mirror={1}
+              mixBlur={0.8}
+              mixContrast={1}
+              mixStrength={80}
+              modelSrc={avatar}
+              onLoaded={function noRefCheck() {}}
+              onLoading={function noRefCheck() {}}
+              reflectorOffset={0}
+              resolution={512}
+              roughness={1}
+              scale={1}
+              style={{}}
+            >
+              <FloorReflection
+                animationSrc="https://readyplayerme.github.io/visage/male-idle.glb"
+                backLightColor="#FFB878"
+                backLightIntensity={2.2}
+                background={{
+                  color: "rgb(9,20,26)",
+                }}
+                bloom={{
+                  intensity: 0.1,
+                  kernelSize: 1,
+                  luminanceSmoothing: 1,
+                  luminanceThreshold: 1,
+                  materialIntensity: 3.3,
+                  mipmapBlur: true,
+                }}
+                blur={[300, 200]}
+                cameraInitialDistance={3.2}
+                cameraTarget={1.55}
+                color="rgb(9,20,26)"
+                depthScale={1.2}
+                depthToBlurRatioBias={1}
+                distortion={0}
+                environment="warehouse"
+                fillLightColor="#6794FF"
+                fillLightIntensity={0.8}
+                fov={50}
+                keyLightColor="#FFFFFF"
+                keyLightIntensity={1.2}
+                maxDepthThreshold={1.4}
+                metalness={0.5}
+                minDepthThreshold={0.4}
+                mirror={1}
+                mixBlur={0.8}
+                mixContrast={1}
+                mixStrength={80}
+                modelSrc={avatar}
+                onLoaded={function noRefCheck() {}}
+                onLoading={function noRefCheck() {}}
+                reflectorOffset={0}
+                resolution={512}
+                roughness={1}
+                scale={1}
+                style={{}}
+              />
+            </Avatar>
+          </div>
+          <div className="text-center font text-2xl font-bold">{id}</div>
+          <div className="flex justify-around">
+            <div>
+              Points
+              <div className="bg-[#EBEBEB] w-[150px] h-[50px] text-center flex items-center justify-center rounded drop-shadow-sm text-4xl">
+                1225
+              </div>
+            </div>
+            <div>
+              Rating
+              <div className="bg-[#EBEBEB] w-[150px] h-[50px] text-center flex items-center justify-center rounded drop-shadow-sm text-4xl">
+                1225
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>
+            Challenges yearly review
+            <HeatMap
+              value={value}
+              weekLabels={["", "Mon", "", "Wed", "", "Fri", ""]}
+              startDate={new Date("2023/01/01")}
+              style={{ width: "500px" }}
+              rectSize={15}
+              rectProps={{
+                rx: 2,
+              }}
+              panelColors={{
+                6: "#7D538D",
+                4: "#BA68C8",
+                2: "#E0BBE6",
+              }}
+            />
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,16 +6,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Challenge from "./components/Challenge.tsx";
 import Marketplace from "./components/Marketplace.tsx";
 import Navbar from "./components/Navbar.tsx";
+import Profile from "./components/Profile.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/challenge" element={<Challenge />} />
         <Route path="/marketplace" element={<Marketplace />} />
+        <Route path="/user/:id" element={<Profile />} />
       </Routes>
-      <Navbar />
     </BrowserRouter>
   </React.StrictMode>
 );
